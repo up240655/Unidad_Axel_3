e4e80ee5dc19bdf67d11d050f1be13a59b47975a
Axel Montes <up240655@alumnos.upa.edu.mx>
2025-03-03 15:19:43 -0600

    Add files via upload

---
+++ 01_Day_Exersice.py
@@ -0,0 +1,14 @@
+2
+print(4 + 5)
+print(8 - 2)
+print(6 * 2)
+print(8 / 4)
+print(6 ** 3)
+print(5 % 3)
+print(5 // 9)
+
+3
+print('Name: Irving Axel ')
+print('Family name: Montes Vazquez')
+print('Contry: Mexico')
+print('I am having a blast learning python')
