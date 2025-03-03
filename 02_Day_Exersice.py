5fb4b87c150b6b784d07b9ea98ba86bc849bd431
Axel Montes <up240655@alumnos.upa.edu.mx>
2025-03-03 15:22:57 -0600

    Add files via upload

---
+++ 02_Day_Exersice.py
@@ -0,0 +1,47 @@
+print('Hello, World!')
+print(len('Hello, World!'))
+print(type('Hello, World'))
+input('Enter your name')
+
+
+first_name = 'Irving'
+last_name ='Montes Vazquez'
+country = 'mexico'
+city = 'Aguascalientes'
+age = '18'
+is_married = False
+skills = ['phython, cnc']
+
+person_info = {
+   'firstname':'Irving',
+   'lastname':'Montes Vazquez',
+   'country':'Mexico',
+   'city':'Aguascalientes'
+   }
+
+  
+
+
+ 
+
+
+print('Hello, world!') 
+print('Hello',',', 'world','!') 
+print(len('Hello world!'))
+
+print('First name:', first_name)
+print('First name length:', len(first_name))
+print('Last name: ', last_name)
+print('Last name length: ', len(last_name))
+print('Country: ', country)
+print('City: ', city)
+print('Age: ', age)
+print('Married: ', is_married)
+print('Skills: ', skills)
+print('Person information: ', person_info)
+
+first_name = input('what is your name: ')
+age = input('How old are you? ')
+
+print(first_name)
+print(age)
