564f2cb0a795d1f965a15519cd1133b10c802100
Axel Montes <up240655@alumnos.upa.edu.mx>
2025-03-05 15:30:34 -0600

    Add files via upload

---
+++ 04_Day_Exersice.py
@@ -0,0 +1,53 @@
+#Exercise 1
+
+No1 = "Thirty"
+No2 = "Days"
+No3 = "Of"
+No4 = "Python"
+Space = " "
+Full = No1 +Space +No2 +Space +No3 +Space +No4
+print(Full)
+print(len(Full))
+
+#Exersice 2
+
+no1 = "Coding"
+no2 = "For"
+no3 = "All"
+space =" "
+full = no1 +space +no2 +space +no3 
+print(full)
+print(len(full))
+
+#Exersice 3
+
+company = "Coding For All"
+
+#Excersice 4
+
+print(company)
+
+#Exersice 5
+
+print(len(company))
+
+#Exersice 6
+
+print(company.upper())
+
+#Exersice 7
+
+print(company.lower())
+
+#Exersice 8
+
+string = "Coding Foe All"
+print(string.capitalize())
+print(string.title())
+print(string.swapcase())
+
+#Exersice 9
+
+print(company[0:6])
+
+#Exersice 10
