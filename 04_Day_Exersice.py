9522b9dcda6d7f9cd89dfb50ac8aec49fb414a0c
Axel Montes <up240655@alumnos.upa.edu.mx>
2025-03-10 15:10:55 -0600

    Add files via upload

--- 04_Day_Exersice.py
+++ 04_Day_Exersice.py
@@ -21,7 +21,7 @@ print(len(full))
 
 #Exersice 3
 
-company = "Coding For All"
+company = 'Coding For All'
 
 #Excersice 4
 
@@ -51,3 +51,107 @@ print(string.swapcase())
 print(company[0:6])
 
 #Exersice 10
+
+oracion = 'Python For Everyone'
+print(oracion.replace('Everyone', 'All'))
+
+#Exersice 11
+
+print(company.split())
+
+#Exersice 12
+
+empresas = "Facebook, Google, Microsoft, IBM, Apple, Oracle, Amazon"
+print(empresas.split(', '))
+
+#Exersice 13
+
+print(company[0])
+
+#Exersice 14
+
+print(len(company) - 1)
+
+#Exersice 15
+
+print(company[10])
+
+#Exersice 16
+
+acronimo1 = ''.join([palabra[0]for palabra in 'Python For Everyone'.split()])
+print(acronimo1)
+
+#Exersice 17
+
+acronimo2 = ''.join([palabra[0] for palabra in 'Coding For All'.split()])
+print(acronimo2)
+
+#Exersice 18
+
+print(company.index('C'))
+
+#Exersice 19
+
+print(company.index('F'))
+
+#Exersice 20
+
+print(company.rfind('1'))
+
+#Exersice 21
+
+oracion = 'You cannot end a sentence with because because because is a conjuction'
+print(oracion.find('because'))
+
+#Exersice 22
+
+print(oracion.rindex('because'))
+
+#Exersice 23
+
+print(oracion[34:57])
+
+#Exersice 24
+
+print(company.startswith('Coding'))
+
+#Exersice 25
+
+print(company.endswith('Coding'))
+
+#Exersice 26
+
+print('    Coding For All    '.strip())
+
+#Exersice 27
+
+print('30DaysOfPython'.isidentifier())
+print('thirty_days_of_python'.isidentifier())
+
+#Exersice 28
+
+bibliotecas_python = ['Django', 'Flask', 'Bottle', 'Pyramid', 'Falcon']
+print(' # '.join(bibliotecas_python))
+
+#Exersice 29
+
+print("I am enjoy this challenge.\nI just wonder what is next")
+
+#Exerisice 30
+
+print("Name\tAge\tCountry\tCity\tAsabeneh\t250\tFinland\tHelsinki")
+
+#Exersice 31
+
+radio = 10
+area = 3.14 * radio **2
+
+#Exersice 32
+
+print(f"8 + 6 = {8 + 6}")
+print(f"8 - 6 = {8 - 6}")
+print(f"8 * 6 = {8 * 6}")
+print(f"8 / 6 = {8 / 6}")
+print(f"8 % 6 = {8 % 6}")
+print(f"8 // 6 = {8 // 6}")
+print(f"8 ** 6 = {8 ** 6}")
